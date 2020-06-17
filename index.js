7f904b6e029cac0684e36967135e2891ca77e353
Myeongwon André Jeon <andrejeon@Myeongwons-MBP.fios-router.home>
2020-06-17 19:29:53 -0400

    mvp complete

--- index.js
+++ index.js
@@ -94,6 +94,30 @@ function Car(model, milesPerGallon) {
   this.odometer = 0;
 }
 
+Car.prototype.fill = function(gallons){
+  return this.tank += gallons;
+};
+
+Car.prototype.drive = function(distance){
+
+  const range = this.tank * this.milesPerGallon;
+
+  if (range > distance){
+    this.odometer += distance;
+    this.tank = this.tank - (distance / this.milesPerGallon);
+  }else{
+    this.odometer += range;
+    this.tank = 0;
+    return `I ran out of fuel at ${this.odometer} miles!`;
+  }
+};
+
+const carOne = new Car('Model A', 20);
+
+console.log(carOne.fill(10));
+console.log(carOne.tank);
+console.log(carOne.drive(50));
+
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
@@ -101,18 +125,31 @@ function Car(model, milesPerGallon) {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age, favoriteToy);
+  this.name = name,
+  this.age = age,
+  this.favoriteToy = favoriteToy
 }
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function play(favoriteToy){
+  return `Playing with ${this.favoriteToy}`;
+}
+
+const babyOne = new Baby('Gabe', 2, 'trains');
+
+console.log(babyOne.play('trains'));
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window Binding
+  2. Implicit Binding
+  3. Explicit Binding
+  4. New Binding
 */
 
 
